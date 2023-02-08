ec85a9df926918faa81e7abb33ec9f295445fe91
RanningMan <xiaranforusa@gmail.com>
2023-02-07 18:42:03 -0800

    fix build

--- src/components/ExtensionFooter/ExtensionFooter.tsx
+++ src/components/ExtensionFooter/ExtensionFooter.tsx
@@ -11,7 +11,7 @@ export default function ExtensionFooter({ locale }: HeaderProps) {
 			{locale === 'en-US'
 				? 'Check out past CSS coffee from '
 				: '查看更多'}
-			<a href={configs.endpoint} target='_blank' rel='noopener'>
+			<a href={configs.endpoint} target='_blank' rel='noreferrer'>
 				{configs.endpoint}
 			</a>
 		</div>
