f0a5b93aa3ef46a586cb527e00dea70977f54cd6
RanningMan <xiaranforusa@gmail.com>
2023-02-13 18:17:51 -0800

    Add clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,17 @@ export default function Document() {
 					rel='stylesheet'
 					href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0&display=swap'
 				/>
+				<Script
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", 'ftv8n00kt9');`,
+					}}
+				/>
 			</Head>
 			<body>
 				<Main />
