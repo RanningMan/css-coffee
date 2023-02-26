573fdf8625e9aaf0dde1b389052011f97f4fca1b
RanningMan <xiaranforusa@gmail.com>
2023-02-26 12:50:59 -0800

    Add chrome extension download link

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -20,13 +20,24 @@ export default function Hero({ locale }: HeroProps) {
 					fill
 				></Image>
 			</div>
-			<a
-				className={styles.button}
-				href='https://github.com/RanningMan/css-coffee'
-			>
-				<span className='material-symbols-outlined'>star</span>
-				{locale === 'en-US' ? `Star on Github` : '在GitHub上关注'}
-			</a>
+			<div className={styles.buttonList}>
+				<a
+					className={styles.button}
+					href='https://github.com/RanningMan/css-coffee'
+				>
+					<span className='material-symbols-outlined'>star</span>
+					{locale === 'en-US' ? `Star on Github` : '在GitHub上关注'}
+				</a>
+				<a
+					className={styles.button}
+					href='https://chrome.google.com/webstore/detail/css-coffee/obgkjajddjldijbjefcckkgnlkogihec'
+				>
+					<span className='material-symbols-outlined'>download</span>
+					{locale === 'en-US'
+						? `Install Chrome PlugIn`
+						: '安装Chrome插件'}
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,36 +1,36 @@
 import { Post } from 'components/components.interface';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { getDaysAgo } from 'utils/getDates';
 import getPosts from 'utils/getPosts';
 
 type Data = {
-  content?: Post[];
-  error?: string;
-}
+	content?: Post[];
+	error?: string;
+};
 
 // GET posts?start=${startday}&limit={number}&locale={locale}
 // today start is 0
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-    const {start, limit, locale} = req.query;
-    if(req.method !== 'GET') {
-        res.status(404).json({error: 'Not Implemented!'});
-    }
-    if(!locale || Array.isArray(locale)) {
-        res.status(404).json({error: 'Invalid query param!'});
-    } else if(!start || Array.isArray(start)) {
-        res.status(404).json({error: 'Invalid query param!'});
-    } else if(!limit || Array.isArray(limit)) {
-        res.status(404).json({error: 'Invalid query param!'});
-        return;
-    } else {
-        let startInt = parseInt(start);
-        let limitInt = parseInt(limit);
-        let endDay = getDaysAgo(startInt);
-        let startDay = getDaysAgo(startInt + limitInt);
-        const posts = await getPosts(startDay, endDay, locale);
-        res.status(200).json({ content: posts });
-    }
+	const { start, limit, locale } = req.query;
+	if (req.method !== 'GET') {
+		res.status(404).json({ error: 'Not Implemented!' });
+	}
+	if (!locale || Array.isArray(locale)) {
+		res.status(404).json({ error: 'Invalid query param!' });
+	} else if (!start || Array.isArray(start)) {
+		res.status(404).json({ error: 'Invalid query param!' });
+	} else if (!limit || Array.isArray(limit)) {
+		res.status(404).json({ error: 'Invalid query param!' });
+		return;
+	} else {
+		let startInt = parseInt(start);
+		let limitInt = parseInt(limit);
+		let endDay = getDaysAgo(startInt);
+		let startDay = getDaysAgo(startInt + limitInt);
+		const posts = await getPosts(startDay, endDay, locale);
+		res.status(200).json({ content: posts });
+	}
 }
